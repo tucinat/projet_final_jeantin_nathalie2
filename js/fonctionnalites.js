056e1a55699e252a1c6bc9c216faa4f65eb21719
tucinat <tucinathalie@gmail.com>
2023-12-14 17:48:58 -0600

    Page panier et fichier js, remplacement

---
+++ js/fonctionnalites.js
@@ -0,0 +1,16 @@
+// ÉVÉNEMENT : écoute de la saisie de l utilisateur et recopie selon la police choisie
+
+var messageInput = document.getElementById('message');
+var textMiroir = document.getElementById('textMiroir');
+var radioTimesNewRoman = document.getElementById('timesNewRoman');
+var radioArial = document.getElementById('arial');
+
+messageInput.addEventListener('input', function () {
+  var selectedFont = radioTimesNewRoman.checked ? 'Times New Roman' : 'Arial';
+  textMiroir.style.fontFamily = selectedFont;
+  textMiroir.innerText = messageInput.value;
+});
+
+// Définition de la police par défaut
+radioTimesNewRoman.checked = true;
+textMiroir.style.fontFamily = 'Times New Roman';
